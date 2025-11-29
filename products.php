51f407a1fbc7219a7d9c64a158e3c30149c488c8
meldrich <meldrich.jose@gmail.com>
2025-11-29 14:51:33 +0800

    Add files via upload

---
+++ products.php
@@ -0,0 +1,22 @@
+<?php
+// Joze Track Sport Store Database
+
+$storeName = "Joze Sports Store";
+$discount = 0.10;
+
+// Joze Watches Data
+$watches = [
+    ["Joze Forerunner 55", 10995, 12],
+    ["Joze Venu Sq 2", 16995, 8],
+    ["Joze Instinct 2 Solar", 25995, 5],
+    ["Joze Fenix 7S", 41995, 4]
+];
+
+// Joze Shoes Data
+$shoes = [
+    ["Joze Runner X1", 3495, 20],
+    ["Joze Sprint Pro", 4995, 15],
+    ["Joze UltraTrail Max", 5995, 10],
+    ["Joze LightStep 2.0", 2595, 25]
+];
+?>
