818e3cd3be8f48eefb20de61d5db9c8cd9c9dae8
meldrich <meldrich.jose@gmail.com>
2025-12-03 21:30:33 +0800

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <footer style="text-align:center; margin-top:30px;">
-    <p>&copy; <?php echo date("Y"); ?> Joze Sports Store | Created by: JOSE, Adrian Meldrich C.</p>
+    &copy; <?= date("Y"); ?> Joze Sports Store | Created by: JOSE, Adrian Meldrich C.
 </footer>
 </body>
 </html>
+
